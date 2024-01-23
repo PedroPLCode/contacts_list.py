2f77a14eb5e337d7165910bf37dfc86a53cb46a4
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2024-01-23 22:20:02 +0100

    Add decorator create_contacts_and_count_time()

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 from base_contact import BaseContact
 from bussiness_contact import BusinessContact
 from faker import Faker
+import time
 
 fake = Faker()
-contacts_list = []
 
-def create_contacts(array, contact_type='private', how_many=1):
+def create_contacts_and_count_time(func):
+    start_time = time.perf_counter()
+    def wrapper(contact_type, how_many):
+        result = func(contact_type, how_many)
+        return result
+    end_time = time.perf_counter()
+    run_time = (end_time - start_time)
+    print(f"Function {func.__name__}() executed in {run_time:.6f}s")
+    return wrapper
+
+@create_contacts_and_count_time
+def create_contacts(contact_type='private', how_many=1):
     if contact_type == 'private' or contact_type == 'bussiness':
+        array = []
         for index in range(0, how_many):
             array.append(
                 create_fake_base_contact_instance(index) if contact_type == 'private' 
                 else create_fake_bussiness_contact_instance(index)
                 )    
+        return array
     else:
         print("Error. Wrong contact type. Only private or bussiness.")
+        return False
 
 def create_fake_bussiness_contact_instance(index_in_array):
     return BusinessContact(
@@ -37,9 +51,12 @@ def create_fake_base_contact_instance(index_in_array):
         )
         
 def show_persons_in_array(array):
-    for person in array:
-        print(person)
-        
-create_contacts(contacts_list, 'private', 512)
-create_contacts(contacts_list, 'bussiness', 128)
+    if not array:
+        print("Error. Array in not iterable.")
+    else: 
+        for person in array:
+            print(person)
+    
+
+contacts_list = create_contacts('bussiness', 1024)
 show_persons_in_array(contacts_list)
